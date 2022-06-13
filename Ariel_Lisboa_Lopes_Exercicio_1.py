504fbd7eb68e88cab1b5b5411f92b0a2a563c2c3
Novo de novo <novodenovo2019@gmail.com>
2022-06-13 09:59:17 -0300

    Atividade 1 finalizada

--- Ariel_Lisboa_Lopes_Exercicio_1.py
+++ Ariel_Lisboa_Lopes_Exercicio_1.py
@@ -1,5 +1,15 @@
-def Campeões (nome_do_time):
-    print (nome_do_time, "é um grande campeão Brasileiro" )
+time_de_futebol = input('Qual é o seu time?: ')  # variavel de escopo global
 
-Campeões ()
-Campeões ()
+
+def func1():         # primeira função
+    if time_de_futebol == "Flamengo":        # condicioanais 
+        print(f'{time_de_futebol} é campeão mundial')
+    else:
+        print(f'{time_de_futebol} não é campeão mundial')
+
+def func2():   # segunda função
+   
+    time_de_futebol = 'Palmeiras'     # escopo local / a variável muda de valor somente dentro dessa função
+    print(f'{time_de_futebol} não é campeão mundial')
+func1()
+func2()
